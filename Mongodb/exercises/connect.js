edf761fc14e9e206a979435292901dd79eb8abf2
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2021-11-12 22:02:10 +0200

    poistettu mongodb

--- Mongodb/exercises/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
-
-const connect = (url) => mongoose.connect(url, {
-  useNewUrlParser: true
-})
-
-module.exports = connect
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # My FrontendMasters courses codes
 
-* Introduction to MongoDB ( kesken: aloita uudelleen 2021.11 )
 * Introduction to Serverless Functions ( aloitettu: 2021.11 )
